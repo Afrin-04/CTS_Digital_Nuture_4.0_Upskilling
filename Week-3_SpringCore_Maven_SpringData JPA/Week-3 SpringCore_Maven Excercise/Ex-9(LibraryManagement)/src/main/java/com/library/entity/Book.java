63f03efce9ae7cd8839aa2933bf788ee804c746a
Afrin-04 <afrinsaleem745@gmail.com>
2025-07-05 20:49:32 +0530

    Initial commit

---
+++ Week-3_SpringCore_Maven_SpringData JPA/Week-3 SpringCore_Maven Excercise/Ex-9(LibraryManagement)/src/main/java/com/library/entity/Book.java
@@ -0,0 +1,34 @@
+package com.library.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
+
+
+
--- Week-3_SpringCore_Maven_SpringData JPA/Week-3 SpringCore_Maven Excercise/Ex-9(LibraryManagement)/src/test/java/com/library/LibraryManagementApplicationTests.java
+++ Week-3_SpringCore_Maven_SpringData JPA/Week-3 SpringCore_Maven Excercise/Ex-9(LibraryManagement)/src/test/java/com/library/LibraryManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.LibraryManagement;
+package com.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
