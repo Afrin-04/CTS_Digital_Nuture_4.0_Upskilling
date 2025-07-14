404acf10e6106b2042ab84bfbe3c1ef649adfaba
Afrin-04 <afrinsaleem745@gmail.com>
2025-07-14 13:29:02 +0530

    Initial commit

--- Week-4_Spring_Rest/1.Springlearn/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO country (code, name) VALUES ('IN', 'India');
-INSERT INTO country (code, name) VALUES ('US', 'United States');
-INSERT INTO country (code, name) VALUES ('DE', 'Germany');
-INSERT INTO country (code, name) VALUES ('JP', 'Japan');
